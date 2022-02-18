552937e072c6f68d5ea8b2135e65433ff5f0b01c
SJCMN <sam.clavette@gmail.com>
2022-02-18 09:34:11 -0600

    conditional render chars if match or not

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,26 +2,44 @@ import { Component } from '@angular/core';
 import { faker } from '@faker-js/faker';
 
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'TYPING_APP';
-  actionCall = 'Test Your Typing Skill!'
+  actionCall = 'Test Your Typing Skill!';
   randomText = faker.lorem.sentence();
+  randomLetter = '';
   inputText = '';
-  textMatch = false;
 
-  updateLorem(){
+  // textMatch = false;
+
+  updateLorem() {
     this.randomText = faker.lorem.sentence();
-  };
+    // updateLorem on enter
+    // clear input on enter || textMatch
+    
+  }
 
-  onTypeCheck(value: string){
+  onInput(value: string) {
     console.log(value);
     this.inputText = value;
-   
   }
+
+  checkType(randomLetter: string, inputLetter: string) {
+
+    if (!inputLetter) {
+      return 'has-text-black';
+    } 
+    
+    return inputLetter === randomLetter ? 'has-text-primary' : 'has-text-danger'
+ 
+  }
+
+  // alternative comparison
+  // if (value === this.inputText){
+  //   this.textMatch = !this.textMatch;
+  // }
 }
